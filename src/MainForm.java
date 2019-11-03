c242f130124d364131bbbd1e75be7995dadcb8c3
lroberto68 <roberto_luongo@live.com>
2019-11-03 18:17:02 +0100

    Inserito controllo input

--- src/MainForm.java
+++ src/MainForm.java
@@ -2,10 +2,12 @@ import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JButton;
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+
 import javax.swing.JLabel;
 
 
@@ -13,6 +15,7 @@ public class MainForm {
 
 	private JFrame frame;
 	private JTextField textField;
+	private JLabel lblNewLabel;
 	private Person p=new Person();
 	/**
 	 * Launch the application.
@@ -36,7 +39,8 @@ public class MainForm {
 	public MainForm() {
 		initialize();
 	}
-
+	
+	
 	/**
 	 * Initialize the contents of the frame.
 	 */
@@ -48,14 +52,14 @@ public class MainForm {
 		
 		JButton btnNewButton = new JButton("Ok");
 
-		JLabel lblNewLabel = new JLabel("New label");
+		lblNewLabel = new JLabel("New label");
 		lblNewLabel.setBounds(65, 141, 70, 15);
 		frame.getContentPane().add(lblNewLabel);
 		
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				p.setEta(Integer.parseInt(textField.getText()));
-				lblNewLabel.setText(Integer.toString(p.getEta()));
+				btnNewButtonActionPerformed(e);
+				
 			}
 		});
 		btnNewButton.setBounds(288, 36, 117, 25);
@@ -66,5 +70,17 @@ public class MainForm {
 		frame.getContentPane().add(textField);
 		textField.setColumns(10);
 		
+		
+	}
+	
+	private void btnNewButtonActionPerformed(ActionEvent e){
+		try{
+			p.setEta(Integer.parseInt(textField.getText()));
+			lblNewLabel.setText(Integer.toString(p.getEta()));
+		}
+		catch(Exception e1){
+			JOptionPane.showMessageDialog(null,"il valore inserito non è numerico","errore",JOptionPane.ERROR_MESSAGE);
+			//e1.printStackTrace();
+		}
 	}
 }
